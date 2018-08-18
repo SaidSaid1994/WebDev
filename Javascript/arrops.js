2356ed27bd2447557007649c3a1411b6c83d5c1f
SaidSaid1994 <42245646+SaidSaid1994@users.noreply.github.com>
2018-08-18 21:02:27 +0100

    Add files via upload

---
+++ Javascript/arrops.js
@@ -0,0 +1,17 @@
+function arrayReverse(arr)
+{
+     var newString = "";
+ 
+
+    for (var i = arr.length - 1; i >= 0; i--)
+	{ 
+        newString += str[i]; 
+    }
+	alert("Reverse String : "+newString)
+}
+
+function arraySize(arr)
+{
+	var len=arr.length;
+	alert("Size of Array : "+len);
+}
